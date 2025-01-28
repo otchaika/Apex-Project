72105cf4fe11fd69f8171d46dfa41e1b20286c69
Koorosh Sabzehzar <Sabzehzarkoorosh@gmail.com>
2025-01-28 22:57:48 +0100

    Optimization
    Optimized the recent changes

--- Assets/Scripts/SFX Manager1.cs
+++ Assets/Scripts/SFX Manager1.cs
@@ -3,56 +3,61 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class SFXManager : MonoBehaviour
+public class SFXManager1 : MonoBehaviour
 {
     [Header("VFX Sounds")]
     [SerializeField] private AudioClip Herb_grinding;
+    [SerializeField] private float Volume1;
     [SerializeField] private AudioClip Coal_grinding;
+    [SerializeField] private float Volume2;
     [SerializeField] private AudioClip Clay_grinding;
+    [SerializeField] private float Volume3;
     [SerializeField] private AudioClip Pigments;
+    [SerializeField] private float Volume4;
     [SerializeField] private AudioClip Picking;
+    [SerializeField] private float Volume5;
     [SerializeField] private AudioClip Paitning;
+    [SerializeField] private float Volume6;
     [SerializeField] private AudioClip PaintSwapper;
+    [SerializeField] private float Volume7;
     [SerializeField] private AudioClip BrushAttaching;
+    [SerializeField] private float Volume8;
     [SerializeField] private AudioClip FullyAssembeled;
+    [SerializeField] private float Volume9;
     [SerializeField] private AudioClip Dropped;
+    [SerializeField] private float Volume10;
     [SerializeField] private AudioClip Water;
-    AudioSource sfx;
+    [SerializeField] private float Volume11;
     // Start is called before the first frame update
-    void Start()
-    {
-        sfx = GetComponent<AudioSource>();
-    }
-
     // Update is called once per frame
     void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Herb"))
         {
-            sfx.clip = Herb_grinding;
-            sfx.PlayOneShot(sfx.clip);
+            AudioSource.PlayClipAtPoint(Herb_grinding, transform.position, Volume1);
+            Destroy(Herb_grinding);
         }
         else if (collision.gameObject.CompareTag("Coal"))
         {
-            sfx.clip = Coal_grinding;
-            sfx.PlayOneShot(sfx.clip);
+            AudioSource.PlayClipAtPoint(Coal_grinding, transform.position, Volume2);
+            Destroy(Coal_grinding);
         }
         else if (collision.gameObject.CompareTag("Clay"))
         {
-            sfx.clip = Clay_grinding;
-            sfx.PlayOneShot(sfx.clip);
+            AudioSource.PlayClipAtPoint(Clay_grinding, transform.position, Volume3);
+            Destroy(Clay_grinding);
         }
         else if (collision.gameObject.CompareTag("Water"))
         {
-            sfx.clip = Water;
-            sfx.PlayOneShot(sfx.clip);
+            AudioSource.PlayClipAtPoint(Water, transform.position, Volume11);
+            Destroy(Water);
         }
         else if (collision.gameObject.CompareTag("Environement"))
         {
             if (collision.relativeVelocity.magnitude > 2)
             {
-                sfx.clip = Dropped;
-                sfx.PlayOneShot(sfx.clip);
+                AudioSource.PlayClipAtPoint(Dropped, transform.position, Volume10);
+                Destroy(Dropped);
             }
         }
 
@@ -62,8 +67,8 @@ public class SFXManager : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Pigments"))
         {
-            sfx.clip = Pigments;
-            sfx.PlayOneShot(sfx.clip);
+            AudioSource.PlayClipAtPoint(Pigments, transform.position, Volume4);
+            Destroy(Pigments);
         }
     }
 }
